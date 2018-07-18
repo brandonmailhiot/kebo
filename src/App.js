1f9f646cbc3ad38799621a33b86ee84e33a2afad
mailhiotb <mailhiotb@Webtrendss-MacBook-Pro.local>
2018-07-18 01:23:01 -0700

    found proper emoji

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <Logo name='🔥 kebo' />
+        <Logo name='🎹 kebo' />
         <Keyboard />
       </div>
     );
